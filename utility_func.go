1a17fbab11de92be1b2a8974cfd3a097c39b47d5
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-08 22:54:28 -0300

    Adjusting CORS for other types of request.

--- utility_func.go
+++ utility_func.go
@@ -13,7 +13,7 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Add("Access-Control-Allow-Origin", "*")
 	// (*w).Header().Add("Access-Control-Allow-Credentials", "true")
 	// (*w).Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-	// (*w).Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, OPTIONS")
 	(*w).Header().Add("Content-Type", "application/json")
 }
 
