a027c4980f344706df2c54ee9c7163e2a0beb5e9
Daniel Alves <danielribeiroalves0@gmail.com>
2022-12-12 21:08:16 -0300

    Adding future Token handler functions

--- utility_func.go
+++ utility_func.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/go-sql-driver/mysql"
 )
 
@@ -35,4 +38,57 @@ func dbConn() (db *sql.DB) {
 	}
 
 	return
+}
+
+// Still need to be corrected
+func checkTokenHandler(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+		bearerToken := strings.Split(header, " ")
+		if len(bearerToken) != 2 {
+			http.Error(w, "Cannot read token", http.StatusBadRequest)
+			return
+		}
+		if bearerToken[0] != "Bearer" {
+			http.Error(w, "Error in authorization token. it needs to be in form of 'Bearer <token>'", http.StatusBadRequest)
+			return
+		}
+		token, ok := checkToken(bearerToken[1])
+		if !ok {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if ok && token.Valid {
+			username, ok := claims["username"].(string)
+			if !ok {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			//check if username actually exists
+			if _, ok := users[username]; !ok {
+				http.Error(w, "Unauthorized, user not exists", http.StatusUnauthorized)
+				return
+			}
+			//Set the username in the request, so I will use it in check after!
+			context.Set(r, "username", username)
+		}
+		next(w, r)
+	}
+}
+
+func checkToken(tokenString string) (*jwt.Token, bool) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(getSecret()), nil
+	})
+	if err != nil {
+		return nil, false
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return nil, false
+	}
+	return token, true
 }
